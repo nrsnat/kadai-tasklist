94675fa6a4344729ebc2a6f9b695b34498c1298c
nrs.nat <nrsnat90@gmail.com>
2023-03-22 01:02:04 +0000

    CRUD tasks

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,25 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 Illuminate\Support\Facades\Facade\Route.phpを利用する、ということを宣言しています。
 Illuminate というのは、 vendor/laravel/framework/src/Illuminate/ に存在します。
+*/
+use App\Http\Controllers\TasksController;
 
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+Route::get('/', [TasksController::class, 'index']);
+
+/*
+Route::get('tasks/{id}', [TasksController::class, 'show']);
+Route::post('tasks', [TasksController::class, 'store']);
+Route::put('tasks/{id}', [TasksController::class, 'update']);
+Route::delete('tasks/{id}', [TasksController::class, 'destroy']);
+
+Route::get('tasks', [TasksController::class, 'index'])->name('task.index');
+Route::get('tasks/create', [TasksController::class, 'create'])->name('task.create'); //違うかも
+
+Route::get('tasks/{id}/edit', [TasksController::class, 'edit'])->name('task.edit');
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::resource('tasks', TasksController::class);
